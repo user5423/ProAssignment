7c799588437d4e6abcd2cbb3bd74dc22f8d9b7de
user5423 <davide.correia@outlook.com>
2021-01-12 14:14:49 +0000

    Slow update

---
+++ public/index.js
@@ -0,0 +1,318 @@
+// Now that we have a base page to work with, we need to start building the base systems
+// We need to build a function that requests and updates content on the DOM
+
+//Setting global variables
+const eventToGetRequest = {"runningScans": "/runningScans",
+                            "scanReports": "/scanReports",
+                            "networkScanner": "/networkScanner",
+                            "dnsScanner": "/dnsScanner",
+                            "index": "/index"};
+
+const eventToPostRequest = {"submitScanForm": submitScanForm};
+
+const responseHandlingDefinitions = {"networkScanner": [jsonParse, createScanPage],
+                                    "dnsScanner": [jsonParse, createScanPage],
+                                    "index": [htmlParse, updateBody],
+                                    "runningScans": [jsonParse, createRunningReport],
+                                    "finishedScans": [jsonParse, createFinishedReport]};
+
+
+var domEventDefinitions = {"type": {"radio": setRadioStatus,
+                                    "checkbox": setCheckboxStatus},
+                           "class": {"card": toggleAccordionVisibility}
+                        };
+
+
+
+var currentLoadedPage = "index";
+
+
+//Explain decision to use click event listener
+window.addEventListener("click", event => {
+    var resourcePath, eventTarget, eventTargetID;
+
+    //Event targets are just node interface objects with extended methods for event handling
+    eventTarget = event.target;
+    // We need to define how the definitions below will work
+    eventTargetID = eventTarget.id;
+
+    if ((resourcePath = eventToGetRequest[eventTargetID]) != undefined) {
+        updateDomByGetRequest(eventTargetID, resourcePath);
+        currentLoadedPage = eventTargetID;
+    } 
+    
+    else if ((resourcePath = eventToPostRequest[eventTargetID]) != undefined) {
+        submitScanForm(currentLoadedPage);
+    }
+    else {
+        checkDomEvents(eventTarget);
+        console.log("This event id isn't registered");
+    }
+
+})
+
+
+function checkDomEvents(eventTarget){
+    for(var key in domEventDefinitions){
+        for (var value in domEventDefinitions[key]){
+            if (key == "class"){
+                if (eventTarget.classList.contains(value)){
+                    domEventDefinitions[key][value](eventTarget);
+                }
+            }
+            else if (eventTarget[key] == value) {
+                domEventDefinitions[key][value](eventTarget);
+                return true;
+            }
+        }
+
+    }
+    return false;
+}
+
+
+function jsonParse(response){ return response.json();}
+
+function htmlParse(response){ return response.text();}
+
+
+function updateDomByGetRequest(eventTargetID, resourcePath){
+    resourcePath = `http://127.0.0.1:4444${resourcePath}`;
+    try {
+        fetch(resourcePath)
+        .then(response => responseHandlingDefinitions[eventTargetID][0](response))
+        .then(body => {
+            responseHandlingDefinitions[eventTargetID][1](body);
+        })
+
+    } catch(err) {
+        console.log(err);
+        document.getElementsById("body").innerHTML = "<b>We were unable to load the page</b>";
+    }   
+
+}
+
+
+function updateBody(body){
+    var parser = new DOMParser();
+    var htmlDoc = parser.parseFromString(body, 'text/html');
+    document.getElementById("body").innerHTML = htmlDoc.getElementById("body").innerHTML;
+}
+
+
+function submitScanForm(){
+    //First we need to compile the form
+    var formJson = {};
+    var input, label, item;
+
+    //Every scan has a host input, so we can hardcode this in
+    var hostInput = document.getElementById("inputHost").value;
+    formJson["host"] = hostInput;
+
+    //This gets the active checkboxes and radioboxes
+    var formCheckCollection = document.getElementsByClassName("form-check");
+    for(var i = 0; i < formCheckCollection.length; i++){
+        item = formCheckCollection[i];
+        input = item.children[0];
+        label = item.children[1];
+
+        if (item.classList.contains("form-check") == true){
+            if (input.value == "true"){
+                formJson[label.getElementsByTagName("b")[0].innerText] = input.value;
+            }
+        }
+        else {
+            formJson[label.innerText] = input.value;
+        }
+        formJson.scanType = currentLoadedPage;
+    }
+
+    
+    console.log(JSON.stringify(formJson));
+    try {
+        fetch(`http://127.0.0.1:4444/${currentLoadedPage}`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json',},
+            body: JSON.stringify(formJson),
+        })
+    }
+    catch(err) {
+        console.log("Unable to submit scan request");
+    }
+}
+
+
+function createScanPage(body){
+    // TODO fomalize howe we add html together
+    var htmlBio = createBio(body["PageStart"]);
+    var htmlForm = createForm(body["FormMethods"]);
+
+    var bodyContainer = document.createElement("div");
+    bodyContainer.setAttribute("class", "row");
+
+    appendInnerHTML(bodyContainer, "<div class=\"col-9\" style=\"padding-top: 10px;\">");
+    bodyContainer.children[0].innerHTML = htmlBio + htmlForm;
+    
+    document.getElementById("body").innerHTML = bodyContainer.innerHTML;
+
+    addFormEventListeners();
+    
+
+}
+
+function addFormEventListeners(){
+    var elementsToListen = document.getElementsByTagName("input");
+    for (var i = 0; i < elementsToListen.length; i++){
+        var element =  elementsToListen[i];
+        if(element.type == 'radio'){ 
+            element.addEventListener("click", setRadioStatus(this));
+        }
+        else if (element.type == "checkbox"){
+            console.log("set");
+            element.addEventListener("click", setCheckboxStatus(this));
+        } else {
+            console.log("wtf");
+        }
+    }
+}
+
+//TODO: CHeck that scanType and hostname are pulling from the right array/obj ref/indexes
+function createRunningReport(body){
+    var scanDescriptor = body["scan descriptor"];
+    var scanResults = body["scan results"];
+
+    var scanType = Object.keys(scanDescriptor[0])[0];
+    var hostname = Object.keys(scanDescriptor[0])[1];
+    var datetime = 0; //TODO: Add datetime to the scan submit
+
+
+    //Let's create the accordion
+    var accordion = document.createElement("div");
+    accordion.classList.add("accordion");
+    accordion.id = "accordion";
+
+    //First we create the scan title for the card
+    var card = document.createElement("div");
+    card.class = "card-header";
+    card.id = "headingOne" // TODO: We need to make this dynamic
+
+    //Building the card title
+    var cardTitle = document.createElement("div");
+    cardTitle.innerHTML = "<h2 class=\"mb-0\"></h2>";
+    cardTitle.children[0].innerHTML = `<div class="btn btn-link btn-block text-left text-light" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style="text-decoration:none"></div>`;
+    cardTitle.children[0].children[0].innerText = `${scanType} on ${hostname} at ${datetime}`;
+
+    //Building card body
+
+    //Scan description
+    var scanDescription = document.createElement("dummy");
+    scanDescription.innerHTML = `<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="accordion"><div class="card-body"></div></div>`;
+    scanDescription = scanDescription.children[0];
+
+    //TODO: Add custom code for the parameters
+    if (scanDescriptor[0]["nmapscan"][1].length > 0){
+        scanDescription.innerHTML = `<h5>Scan Parameters</h5> <b>${scanDescriptor.join("</b><b>")}</b> </h5>\n<hr>`;
+    } else {
+        scanDescription.innerHTML = `<h5>Scan Parameters</h5> <b>Default</b> </h5>\n<hr>`;
+    }
+
+    //Scan results
+    var scanResultHTML = document.createElement("dummy");
+    scanResultHTML.innerHTML = `<table class="table"><tbody></tbody></table>`;
+    scanResultHTML = scanResultHTML.innerHTML;
+    
+    var scanResultKeys = Object.keys(scanDescriptor[0])[0];
+    for (var i = 0; i< scanResults; i++){
+        // if //scanResults
+        // appendInnerHTML(scanResultsHTML.children[0].children[0], `<tr><th scope="row">${}</th><td>${}</td></tr>`);
+
+    }
+
+    return null;
+}
+
+function createFinishedReport(){
+    //
+    return null;
+}
+
+
+function toggleAccordionVisibility(eventTarget){
+    return null;
+}
+
+
+function createBio(htmlString){
+    var divElement = document.createElement("div");
+    divElement.innerHTML = htmlString;
+    return divElement.outerHTML;
+}
+
+
+function createForm(formMethods){ 
+    var formElement = document.createElement("form");
+    var itemCounter = 0;
+    var sectionCounter = 0;
+    var item;
+
+    for(var section in formMethods) {
+        sectionCounter += 1;
+        var formGroup = document.createElement("div");
+        formGroup.setAttribute("class", "form-group");
+        formGroup.innerHTML = `<p><b>${section}</b></p>`;      
+        var itemType = formMethods[section]["type"];
+
+        for (var i = 0; i< formMethods[section]["formItems"].length; i++){
+            item = formMethods[section]["formItems"][i];
+            itemCounter += 1;
+            var formItemHTML = `
+                <div class="form-check">
+                    <input class="form-check-input" type="${itemType}" value="false" id="defaultCheck${itemCounter}" name="section${sectionCounter}" >
+                    <label class="form-check-label" for="defaultCheck${itemCounter}">${item}</label>
+                </div>`;
+
+            appendInnerHTML(formGroup, formItemHTML);
+        }
+
+        appendInnerHTML(formElement, formGroup.outerHTML);
+        appendInnerHTML(formElement, "<br>")
+    }
+
+    // Replaced button with div in order to fix behaviour bug that was caused by bs4's javascript code
+    appendInnerHTML(formElement, `<div id="submitScanForm" class="btn btn-primary">Submit</div>`)
+
+
+    return formElement.outerHTML;
+}
+
+function appendInnerHTML(element, newInnerHTML){
+    var originalInnerHTML = element.innerHTML;
+    element.innerHTML = originalInnerHTML + newInnerHTML;
+}
+
+//They all start as false 
+//This assumes that when they click it
+function setRadioStatus(element){
+    element.value = "true";
+    console.log(element);
+    console.log(element.id);
+    console.log(element.name);
+    var elements = document.getElementsByName(element.name);
+    for (var i = 0; i< elements.length; i++){
+        if(elements[i].value == "true" && elements[i] != element) {
+            elements[i].value = "false";
+        }
+    }
+}
+
+function setCheckboxStatus(element){
+    if (element.value == "true"){
+        element.value = "false";
+    }
+    else {
+        element.value = "true";
+    }
+}
+
+
+
--- server.js
+++ server.js
@@ -1,61 +1,35 @@
 var express = require('express');
 const util = require('util')
 const nmap = require('node-nmap');
+const dns = require('dns');
 // const { report } = require('process');
-
-// var app = express();
-// app.use(express.static('public'));
-// app.get('/', req)
-
 
 var app = express();
 
-
-
-function executeNmapScan(params){
-
-    nmap.nmapLocation = "nmap"; //default
-    var nmapscan = new nmap.NmapScan('google.com', ['-A', '-sV', '-sU']);
-
-    var host = params["host"];
-
-
-    nmapscan.on('complete', data =>
-        {return data;})
-
-    nmapscan.on('error', data=>
-        console.log(data));
-
-
-    nmapscan.startScan();
-}
-
-
 app.use(express.static('public'))
-
 app.use(express.static("assets"))
-
 app.use(express.static("js"))
 
+app.use(express.json());
 
+var runningScans = [];
+var finishedScans = [];
 
+app.get("/index", (req, resp) => resp.sendFile('./public/index.html', { root: __dirname }))
 
-app.get("/runningScans", (req, resp) => {
-    resp.sendFile("runningScans.html", { root: __dirname});
-    console.log(resp);
-})
+app.get("/runningScans", (req, resp) => resp.json(runningScans).send())
 
-
-app.get("/scanReports", (req, resp) => {
-    resp.sendFile("scanReports.html", { root: __dirname});
-    console.log(resp);
-})
-
+app.get("/finishedScans", (req, resp) => resp.json(finishedScans).send())
 
 app.get("/networkScanner", (req, resp) => {
     const nmapMethods = {"PageStart" : `<h3>Network Scanner</h3>
                                         <p>There are numerous tools to help you with you're scan. Since this is a demo site, there isn't an extensive number of supported tools
-                                        provided out of the box. However, there are a few good tools included.</p>`,
+                                        provided out of the box. However, there are a few good tools included.</p>
+                                        <div class="form-group ">
+                                            <label for="inputHost">Host (IP address / Hostname):</label>
+                                            <input type="text" class="form-control" id="inputHost" aria-describedby="emailHelp" placeholder="Enter Host">
+                                            <small id="emailHelp" class="form-text text-muted">Port scanning is not illegal in the UK. But make sure you abide by the rules or ToS of your network and local legislation</small>
+                                        </div><br>`,
                                         
                             
                          "FormMethods": {"Scan Technqiues" : {
@@ -69,8 +43,8 @@ app.get("/networkScanner", (req, resp) => {
                                         "Port Range": {
                                                     "type" : "radio",
                                                     
-                                                    "formItems": ["Default Range (1-1000)",
-                                                                "Full Range (1-65535)"]
+                                                    "formItems": ["<b>-p1-1000</b> : Default Range (1-1000)",
+                                                                "<b>-p-</b> : Full Range (1-65535)"]
                                                 },
 
                                         "Additional Scan Technqiues": {
@@ -87,40 +61,144 @@ app.get("/networkScanner", (req, resp) => {
 // We need to decide how we are going to create the form
 })
 
-app.post("/submitScan", (req, resp) => {
-    console.log(req.body);
-    
+app.post("/networkScanner", (req, resp) => {
+    var formKeys = Object.keys(req.body);
+
+    var hostname = req.body["host"];
+
+    var params = [];
+
+    for(var i = 0; i < formKeys.length; i++){
+        var paramSwitch = formKeys[i];
+        if (paramSwitch != "host" && paramSwitch != "scanType"){
+            params.push(paramSwitch);
+        }
+    }
+    executeNmapScan(hostname, params);
+
+    resp.send();
 })
 
-app.get("/hello", (req, resp) =>
-    resp.send("hello"))
+
+app.get("/dnsScanner", (req, resp) => {
+    const dnsMethods = {"PageStart" : `<h3>DNS Scanner</h3>
+                                        <p>Using OS resolver libraries we are able to query for many different DNS records. Unfortunately,
+                                        due to not knowing what environment our suite is running on, we chose not to implement unix tools
+                                        such as dig.</p>
+                                        <div class="form-group ">
+                                            <label for="inputHost">Host (IP address / Hostname):</label>
+                                            <input type="text" class="form-control" id="inputHost" aria-describedby="emailHelp" placeholder="Enter Host">
+                                            <small id="emailHelp" class="form-text text-muted">Please make sure to point correctly, i.e. if you mean to point to the parent domain
+                                            than do that instead of pointing to a subdomain such as "www"</div><br>`,
+                                        
+                            
+                         "FormMethods": {"Record Types" : {
+                                                    "type" : "checkbox",
+                                                    
+                                                    "formItems" : ["<b>A</b> : IPv4 Address",
+                                                                    "<b>AAAA</b> : IPv6 Address",
+                                                                    "<b>CNAME</b> : Canonical Name Records",
+                                                                    "<b>MX</b> : Mail Exchange Records",
+                                                                    "<b>NS</b> : Name Server Records",
+                                                                    "<b>TXT</b> : Text Records",
+                                                                    "<b>SOA</b> : Start of Authority Records"
+                                                                ]
+                                                }
+                                            }
+
+    };
+
+    resp.json(dnsMethods).send();
+})
 
 
 
-// app.get("/", (req, resp) => {
-//     resp.sendFile("index.html", {root: __dirname});
-//     console.log("yup");
-// })
+app.post("/dnsScanner", (req, resp) => {
+    console.log(req.body);
+    //Here we will grab the formKeys
+    var formKeys = Object.keys(req.body);
 
+    var hostname = req.body["host"];
 
+    var params = [];
 
-app.post("/nmapScan", (req, resp) => {
+    for(var i = 0; i < formKeys.length; i++){
+        var paramSwitch = formKeys[i];
+        if (paramSwitch != "host" && paramSwitch != "scanType"){
+            params.push(paramSwitch);
+        }
+    }
+    executeDNSScan(hostname, params)
+
+    resp.send();
+    //We will then iterate and perfrom
     return null;
 })
 
 
 
-app.get("/index", (req, resp) => {
-    resp.sendFile('./public/index.html', { root: __dirname });
-})
+//TODO: change from array to dictionary as there may be issues if multiple users were using this and the position shifted
+function executeNmapScan(hostname, params){
+    nmap.nmapLocation = "nmap"; //default
+    var nmapscan = new nmap.NmapScan(hostname, params);
+
+    var arrayPosition = logScanAsRunning("nmapscan", hostname, params);
+
+    nmapscan.on('complete', data => {
+        var scanDescriptor = runningScans.splice(arrayPosition, 1);
+        finishedScans.push({"scan descriptor": scanDescriptor,
+                            "scan results": data });
+        console.log(finishedScans);
+    }).on('error', data => {
+        // console.log(data); // TODO: change this so that it reflects in finished scan reports as an error
+        var scanDescriptor = runningScans.splice(arrayPosition, 1);
+        finishedScans.push({"scan descriptor": scanDescriptor,
+                            "scan results": data });
+    });
+
+    nmapscan.startScan();
+}
+
+
+function executeDNSScan(hostname, params){
+    var arrayPosition = logScanAsRunning("dnsscan", hostname, params);
+
+    var results = {};
+    
+    for(var i = 0; i< params.length; i ++) {
+        // console.log(param);
+        var param = params[i];
+        dns.resolve(hostname, param, (err, ret) => {
+            if (ret == true){
+                results[param] = ret;
+    
+            }
+            // console.log(ret);
+            results[param] = ret;
+        })
+    }
+    
+    console.log(results);
+    return null;
+}
+
+
+
+
+
+//Logs the scan as running and returns position in array
+function logScanAsRunning(scanname, hostname, params){
+    runningScans.push({[scanname] : [hostname, params]});
+    return runningScans.length -1;
+}
+
 
 
 app.listen(4444, () => {
     console.log(`Example app listening at http://127.0.0.1:${4444}`)
   })
 
-// app.post('/netscan', (req, resp) => {
-//     var host = req.param["host"];
 
-// })
+
+
 

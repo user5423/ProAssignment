4458b652b46ab85572d699f9c35d6d683b8a4ede
user5423 <davide.correia@outlook.com>
2021-01-18 18:53:03 +0000

    Added input validation

--- public/index.js
+++ public/index.js
@@ -9,7 +9,7 @@ const eventToGetRequest = {"runningScans": "/runningScans",
                             "dnsScanner": "/dnsScanner",
                             "index": "/index"};
 
-const eventToPostRequest = {"submitScanForm": submitScanForm};
+const eventToPostRequest = {"submitScanForm": nsubmitScanForm};
 
 const responseHandlingDefinitions = {"networkScanner": [jsonParse, createScanPage],
                                     "dnsScanner": [jsonParse, createScanPage],
@@ -43,7 +43,7 @@ window.addEventListener("click", event => {
         updateDomByGetRequest(eventTargetID, resourcePath);
         currentLoadedPage = eventTargetID;
     } else if ((resourcePath = eventToPostRequest[eventTargetID]) != undefined) {
-        submitScanForm(currentLoadedPage);
+        nsubmitScanForm(currentLoadedPage);
     } else {
         checkDomEvents(eventTarget);
         console.log("This event id isn't registered");
@@ -113,17 +113,23 @@ function checkDomEvents(eventTarget){
 
 function updateDomByGetRequest(eventTargetID, resourcePath){
     resourcePath = `http://127.0.0.1:4444${resourcePath}`;
-    try {
-        fetch(resourcePath)
-        .then(response => responseHandlingDefinitions[eventTargetID][0](response))
-        .then(body => {
-            responseHandlingDefinitions[eventTargetID][1](body);
-        })
 
-    } catch(err) {
-        console.log(err);
-        document.getElementsById("body").innerHTML = "<b>We were unable to load the page</b>";
-    }   
+    fetch(resourcePath)
+    .then(response => {
+        if (!(response.ok || response.redirected)){
+            throw Error(response.status);
+        }
+        return response;
+    })
+    .then(response => responseHandlingDefinitions[eventTargetID][0](response))
+    .then(body => {
+        responseHandlingDefinitions[eventTargetID][1](body);
+    }).catch(error => {
+        processFetchError(error);
+    });
+
+
+
     return null;
 }
 
@@ -135,15 +141,175 @@ function updateBody(body){
 }
 
 
+function processFetchError(error){
+    // If ther error is not a number there was a networking issue
+    console.log(error);
+    if (isNaN(error)){
+        // document.getElementById("body").innerHTML = `<h4>There was a networking issue that caused the request to fail. This could have been anything from a disconnect to failed DNS lookup</h4>`;
+        document.getElementById("body").innerHTML = `<h4>We detected a ${error} code</h4>`;
+    }
+    else {
+        if (error[0] == "4"){
+            document.getElementById("body").innerHTML = `<h4>We detected a ${error} code - that means that the method you provided wasn't supported </h4>`;
+        } else if (error[0] == "5"){
+            document.getElementById("body").innerHTML = `<h4>We detected a ${error} code - that means that the server encountered a problem during processing the request</h4>`;
+        } else {
+            document.getElementById("body").innerHTML = `<h4>We detected an unusual that wasn't a 4xx or 5xx error</h4>`;
+        }
+    }
 
-
-
-
+}
 
 
 
 
 // Form Methods //
+function nsubmitScanForm(){
+    var input, label, item, hostInput, formGroupCollection, formItemCollection, i, j, formSearch;
+    var formJson = {};
+
+    //Every scan has a host input, so we can hardcode this in
+    hostInput = document.getElementById("inputHost").value;
+    formJson["host"] = hostInput;
+
+    //This gets the active checkboxes and radioboxes
+    formGroupCollection = document.getElementsByClassName("form-group");
+
+    var isValid = true;
+
+    //Iterating over each formgroup
+    for (i = 0; i < formGroupCollection.length; i++){
+        var formGroup = formGroupCollection[i];
+        var isRequired = formGroup.classList.contains("required")
+        
+        
+        formItemCollection = formGroup.getElementsByClassName("form-check");
+        formSearch = formGroup.getElementsByClassName("form-control");
+        //We first start by checking where we have a formSearch or formItemCollection
+        if (formSearch.length != 0){
+            input = formGroup.getElementsByTagName("input")[0];
+            label = formGroup.getElementsByTagName("label")[0];
+            formSearch = formSearch[0];
+            //We start by checking that it isn't empty
+            if (isValidHostname(formSearch)){
+                formJson["host"] = input.value
+            }
+            else {
+                isValid = false;
+            }
+
+        } else {
+            // If we are dealing with a formItemCollection - i.e. a bunch of checboxes and radioboxes
+            console.log(formItemCollection[i]);
+            if (isRequired){
+                if (!isValidItemGroup(formGroup)){
+                    isValid = false;
+                    continue;
+                }
+            }
+
+            console.log(formItemCollection.length);
+            //We iterate of them
+            for (j=0; j < formItemCollection.length; j++){
+                item = formItemCollection[j];
+                input = item.children[0];
+                label = item.children[1];
+
+
+                if (item.classList.contains("form-check") == true){
+                        formJson[label.getElementsByTagName("b")[0].innerText] = input.value;
+                    }
+                    else {
+                        isValid = false;
+                    }
+                }
+            }
+        }
+
+    if (isValid == false){
+        return null;
+    }
+
+
+    formJson.scanType = currentLoadedPage;
+
+    console.log(JSON.stringify(formJson));
+
+    fetch(`http://127.0.0.1:4444/${currentLoadedPage}`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',},
+        body: JSON.stringify(formJson),
+    }).then(response => {
+        if (!(response.ok || response.redirected)){
+            throw Error(response.status);
+        }
+        document.getElementById("submitScanForm");
+        return response;
+    }).catch(error => {
+        processFetchError(error);
+    });
+    
+    //Here we will want to reset the page
+
+
+
+
+    }
+
+
+function isValidItemGroup(checkboxGroup){
+    checkboxGroup = checkboxGroup.getElementsByTagName("input");
+    for(var i = 0; i < checkboxGroup.length; i++){
+        if (checkboxGroup[i].value == "true") {
+            insertMessage(checkboxGroup[0].parentElement.parentElement, `<div class="feedback val-success">You selected at least one value<div>`);
+            // appendInnerHTML(checkboxGroup[0].parentElement.parentElement,`<div class="feedback val-success">You selected at least one value<div>`);
+            return true;
+        }
+    }
+    insertMessage(checkboxGroup[0].parentElement.parentElement, `<div class="feedback val-error">You need to select at least one value<div>`);
+    // appendInnerHTML(checkboxGroup[0].parentElement.parentElement,`<div class="feedback val-error">You need to select at least one value<div>`);
+    return false;
+}
+
+function insertMessage(node, message){
+    try {
+        if (node.lastChild.classList.contains("feedback")){
+            node.lastChild.innerHTML = message;
+        } else {
+            // appendInnerHTML(node,`<div class="feedback val-error">You need to select at least one value<div>`);
+            appendInnerHTML(node, message);
+        }
+    } catch(err){
+        // appendInnerHTML(node,`<div class="feedback val-error">You need to select at least one value<div>`);
+        appendInnerHTML(node, message);
+    }
+}
+
+
+
+
+
+function isValidHostname(formSearch){
+    if (formSearch.value != ""){
+        addSuccessMessage(formSearch, `Correct hostname`);
+        return true;
+    }
+    addErrorMessage(formSearch, `Please enter a valid hostname!`)
+    return false;
+
+}
+
+function addSuccessMessage(formSearch, message){
+    insertMessage(formSearch.parentElement, `<div class="feedback val-success">${message}<div>`);
+}
+
+function addErrorMessage(formSearch, message){
+    insertMessage(formSearch.parentElement, `<div class="feedback val-error">${message}<div>`);
+}
+
+
+
+
 
 
 //TODO: We need to implement both server side and client side implementation
@@ -173,16 +339,20 @@ function submitScanForm(){
     }
 
     console.log(JSON.stringify(formJson));
-    try {
-        fetch(`http://127.0.0.1:4444/${currentLoadedPage}`, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json',},
-            body: JSON.stringify(formJson),
-        })
-    }
-    catch(err) {
-        console.log("Unable to submit scan request");
-    }
+ 
+    fetch(`http://127.0.0.1:4444/${currentLoadedPage}`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',},
+        body: JSON.stringify(formJson),
+    }).then(response => {
+        if (!(response.ok || response.redirected)){
+            throw Error(response.status);
+        }
+        return response;
+    }).catch(error => {
+        processFetchError(error);
+    });
+
 }
 
 function createScanPage(body){
@@ -209,14 +379,18 @@ function createBio(htmlString){
 
 function createForm(formMethods){ 
     var item, itemType, itemCounter, sectionCounter, section;
-    var formElement, formGroup, formItemHTML, i;
+    var formElement, formGroup, formItemHTML, i, isRequired;
 
     formElement = document.createElement("form");
+    // formElement = html2element(`<form`)
     itemCounter = 0, sectionCounter = 0;
 
     for (section in formMethods){
-        formGroup = html2element(`<div class="form-group"><p><b>${section}</b></p></div>`);
         itemType = formMethods[section]["type"];
+        isRequired = formMethods[section]["required"];
+        formGroup = html2element(`<div class="form-group ${isRequired}"><p><b>${section}</b></p></div>`);
+
+        // With item type we can decide checkbox
 
         for (i = 0; i < formMethods[section]["formItems"].length; i++){
             item = formMethods[section]["formItems"][i];

7d038ca8d85572b969ee821ffb4e3dd888a24bc3
user5423 <davide.correia@outlook.com>
2021-01-24 12:05:40 +0000

    Updated /finishedScans test cases

--- app.test.js
+++ app.test.js
@@ -59,13 +59,13 @@ app.listen(4444, () => {
 
 
 describe('Testing the finishedScans endpoint', () => {
-    test("GET /finishedScans succeeds", () => {
+    test("GET /finishedScans - succeeds", () => {
         return request(app)
         .get('/finishedScans')
         .expect('Content-type', /json/);
     });
 
-    test("GET /finishedScans returns JSON", () => {
+    test("GET /finishedScans - returns JSON", () => {
         return request(app)
         .get('/finishedScans')
         .expect('Content-type', /json/);
@@ -73,23 +73,37 @@ describe('Testing the finishedScans endpoint', () => {
     });
 
     //THis should succeed as random url parameters should just be ignored
-    //TODO: We are going to add ways to select certain scans via url query strings
-    test("GET /finishedScans with random parameters", () => {
+    test("GET /finishedScans - invalid parameter with no invalid key", () => {
         return request(app)
-        .get("/finishedScans?q=random&kb=random")
+        .get("/finishedScans?q=random")
         .expect(200);
     });
+
+    test("GET /finishedScans - invalid parameter with no value", () => {
+        return request(app)
+        .get("/finishedScans?fdasfdf")
+        .expect(200);   //This is not a 400 as we are just returning an empty [] as a valid response
+    });
+
+
+    test("GET /finishedScans - Chained parameters", () => {
+        return request(app)
+        .get("/finishedScans?scanname=dnsscan&hostname=google.com")
+        .expect(200);   //This is not a 400 as we remove duplicate parameters
+    });
+
+
 
 })
 
 describe('Testing the /runningScans endpoint', () => {
-    test("GET /runningScans succeeds", () => {
+    test("GET /runningScans - succeeds", () => {
         return request(app)
         .get('/runningScans')
         .expect('Content-type', /json/);
     });
 
-    test("GET /runningScans returns JSON", () => {
+    test("GET /runningScans - returns JSON", () => {
         return request(app)
         .get('/runningScans')
         .expect('Content-type', /json/);
@@ -98,13 +112,48 @@ describe('Testing the /runningScans endpoint', () => {
 
     //THis should succeed as random url parameters should just be ignored
     //TODO: We are going to add ways to select certain scans via url query strings
-    test("GET /runningScans with random parameters", () => {
+    test("GET /runningScans - with random parameters", () => {
         return request(app)
         .get("/runningScans?q=random&kb=random")
         .expect(200);
     });
 
+
+    //THis should succeed as random url parameters should just be ignored
+    test("GET /runningScans - invalid parameter with no invalid key", () => {
+        return request(app)
+        .get("/runningScans?q=random")
+        .expect(200);
+    });
+
+    test("GET /runningScans - invalid parameter with no value", () => {
+        return request(app)
+        .get("/runningScans?fdasfdf")
+        .expect(200);   //This is not a 400 as we are just returning an empty [] as a valid response
+    });
+
+
+    test("GET /runningScans - Chained parameters", () => {
+        return request(app)
+        .get("/runningScans?scanname=dnsscan&hostname=google.com")
+        .expect(200);   //This is not a 400 as we remove duplicate parameters
+    });
+
+
+
+
+
+
+
+
+
+
+
+
 })
+
+
+
 
 
 
@@ -115,8 +164,8 @@ describe('Testing the /runningScans endpoint', () => {
 
 //Any errors created during the actual execution of the scans is put in the reports
 
-describe('Testing the /networkScanner + /dnsScanner endpoint', () => {
-    test("POST /networkScanner + /dnsScanner - bare minimum body parameters", () => {
+describe('Testing the /networkScanner endpoint', () => {
+    test("POST /networkScanner - bare minimum body parameters", () => {
         return request(app)
         .post('/networkScanner')
         .send({
@@ -126,7 +175,7 @@ describe('Testing the /networkScanner + /dnsScanner endpoint', () => {
         .expect(200);
     });
 
-    test("POST /networkScanner + /dnsScanner - url parameters that don't exist", () => {
+    test("POST /networkScanner - url parameters that don't exist", () => {
         return request(app)
         .post('/networkScanner?q=123')
         .send({
@@ -136,7 +185,7 @@ describe('Testing the /networkScanner + /dnsScanner endpoint', () => {
         .expect(200);
 });
 
-    test("POST /networkScanner + /dnsScanner - content-type response is JSON", () => {
+    test("POST /networkScanner - content-type response is JSON", () => {
         return request(app)
         .post('/networkScanner')
         .send({
@@ -146,7 +195,7 @@ describe('Testing the /networkScanner + /dnsScanner endpoint', () => {
         .expect('Content-type', /json/);
     });
 
-    test("POST /networkScanner + /dnsScanner - incorrect scantype", () => {
+    test("POST /networkScanner - incorrect scantype", () => {
         return request(app)
         .post('/networkScanner')
         .send({
@@ -156,7 +205,7 @@ describe('Testing the /networkScanner + /dnsScanner endpoint', () => {
         .expect(400);
     });
 
-    test("POST /networkScanner + /dnsScanner - incorrect host", () => {
+    test("POST /networkScanner - incorrect host", () => {
         return request(app)
         .post('/networkScanner')
         .send({
@@ -166,7 +215,7 @@ describe('Testing the /networkScanner + /dnsScanner endpoint', () => {
         .expect(400);
     });
 
-    test("POST /networkScanner + /dnsScanner - incorrect host and scantype", () => {
+    test("POST /networkScanner - incorrect host and scantype", () => {
         return request(app)
         .post('/networkScanner')
         .send({
@@ -179,26 +228,114 @@ describe('Testing the /networkScanner + /dnsScanner endpoint', () => {
     //The reason why we allow "incorrect methods" as 200 is because the nmap node wrapper is basic and only supports a limited
     //number of flags/switches. However, as it updates, what once was considered incorrect will be allowed by the wrapper
     //Furthermore we deal with these errors quietly by disregarding them on the server.js
-    test("POST /networkScanner + /dnsScanner - correct host and scantype + incorrect methods", () => {
+    test("POST /networkScanner - correct host and scantype + incorrect methods", () => {
         return request(app)
         .post('/networkScanner')
         .send({
             "host": "google.com",
-            "scanType": "dnsscan",
+            "scanType": "nmapscan",
             "sT" : true,
             "sR": true, 
         })
         .expect(200);
     });
 
-    test("POST /networkScanner + /dnsScanner - correct host and scantype + correct methods", () => {
+
+    
+})
+
+
+
+
+
+
+
+describe('Testing the /dnsScanner endpoint', () => {
+    test("POST /dnsScanner - no scan parameters ", () => {
         return request(app)
-        .post(' ')
+        .post('/dnsScanner')
+        .send({
+            "host": "google.com",
+            "scanType": "dnsscan"
+        })
+        .expect(400);
+    });
+
+    //We are just ignoring these and only return 400 if the parameters below are insufficient
+    test("POST /dnsScanner - url parameters that don't exist", () => {
+        return request(app)
+        .post('/dnsScanner?q=123')
         .send({
             "host": "google.com",
             "scanType": "dnsscan",
-            "sT" : true,
-            "sV": true, 
+            "A": "true"
+        })
+        .expect(200);
+});
+
+    test("POST /dnsScanner - content-type response is JSON", () => {
+        return request(app)
+        .post('/dnsScanner')
+        .send({
+            "host": "google.com",
+            "scanType": "dnsscan"
+        })
+        .expect('Content-type', /json/);
+    });
+
+    test("POST /dnsScanner - incorrect scantype", () => {
+        return request(app)
+        .post('/dnsScanner')
+        .send({
+            "host": "",
+            "scanType": "dnsscan"
+        })
+        .expect(400);
+    });
+
+    test("POST /dnsScanner - incorrect host", () => {
+        return request(app)
+        .post('/dnsScanner')
+        .send({
+            "host": "googlecom",
+            "scanType": "dnsscan"
+        })
+        .expect(400);
+    });
+
+    test("POST /dnsScanner - incorrect host and scantype", () => {
+        return request(app)
+        .post('/dnsScanner')
+        .send({
+            "host": "googlecom",
+            "scanType": "dnsscan21321"
+        })
+        .expect(400);
+    });
+
+    //The reason why we allow "incorrect methods" as 200 is because the nmap node wrapper is basic and only supports a limited
+    //number of flags/switches. However, as it updates, what once was considered incorrect will be allowed by the wrapper
+    //Furthermore we deal with these errors quietly by disregarding them on the server.js
+    test("POST /dnsScanner - correct host and scantype + incorrect methods", () => {
+        return request(app)
+        .post('/dnsScanner')
+        .send({
+            "host": "google.com",
+            "scanType": "dnsscan",
+            "Afdsa" : true,
+            "Afdas": true, 
+        })
+        .expect(400);
+    });
+
+    test("POST /dnsScanner - correct host and scantype + correct methods", () => {
+        return request(app)
+        .post('/dnsScanner')
+        .send({
+            "host": "google.com",
+            "scanType": "dnsscan",
+            "AA" : true,
+            "AAAA": true, 
         })
         .expect(200);
     });
@@ -207,14 +344,6 @@ describe('Testing the /networkScanner + /dnsScanner endpoint', () => {
 
     
 })
-
-// // "scanType" :"dnsscan"
-
-// console.log("Ignore the below. We are testing the endpoints, but our code also sets of async operations that can take many minutes\
-//             We aren't testing the endpoint, not the scan execution, so you can just exit early by entering 'CTRL+C'");
-
-
-
 
 
 // runningScans
